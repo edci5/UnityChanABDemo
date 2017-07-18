87b6df4668c84a877c8a5ecdde5912b48224685c
Hiroki Omae <hiroki@unity3d.com>
2017-07-18 20:02:31 +0900

    add loading scene

---
+++ Assets/Editor/BuildFromGraph.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AssetBundles.GraphTool;
+
+public class BuildFromGraph {
+
+    [MenuItem("Demo/Build")]
+	static void Build () {
+        AssetBundleGraphUtility.ExecuteGraph("Assets/Workflow/AB.asset");
+	}
+}
