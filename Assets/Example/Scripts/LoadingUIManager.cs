629cb37019293ff07e491b70b30245aecf81629f
Hiroki Omae <hiroki@unity3d.com>
2017-07-19 09:42:21 +0900

    Update AssetBundleManager

---
+++ Assets/Example/Scripts/LoadingUIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingUIManager : MonoBehaviour {
+
+    public GameObject errorUI;
+
+    void OnSceneLoadError() {
+        errorUI.SetActive (true);
+    }
+
+    void OnSceneLoaded() {
+        gameObject.SetActive (false);
+    }
+}
